b5ee85acab49bc95661a5b73275a61cd0fed7a0b
TrySoony <125113485+TrySoony@users.noreply.github.com>
2025-06-23 17:04:42 +0300

    Add files via upload

---
+++ prizes.js
@@ -0,0 +1,39 @@
+// prizes.js
+const prizes = [
+  { 
+    name: "Nail Bracelet", 
+    starPrice: 100000, 
+    img: "images/nail_bracelet.png" // Локальный путь
+  },
+  { 
+    name: "Bonded Ring", 
+    starPrice: 37500, 
+    img: "images/bonded_ring.png" // Локальный путь
+  },
+  { 
+    name: "Neko Helmet", 
+    starPrice: 14000, 
+    img: "images/neko_helmet.png" // Локальный путь
+  },
+  { 
+    name: "Diamond Ring", 
+    starPrice: 6700, 
+    img: "images/diamond_ring.png" // Для пустого приза картинка не нужна
+  },
+  { 
+    name: "Love Potion", 
+    starPrice: 4200, 
+    img: "images/love_potion.png" // Локальный путь
+  },
+  { 
+    name: "Easter Egg", 
+    starPrice: 1050, 
+    img: "images/easter_egg.png" // Локальный путь
+  },
+  { 
+    name: "Light Sword", 
+    starPrice: 1450, 
+    img: "images/light_sword.png" // Локальный путь
+  },
+
+];
